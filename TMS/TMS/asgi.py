2f2183d308ed301e79adf347e02de3dce626ce78
MisbahK4 <misbahpt6@gmail.com>
2025-09-11 19:55:37 +0530

    Prepare project for deployment: clean, tested, static ASGI.py updated

--- TMS/TMS/asgi.py
+++ TMS/TMS/asgi.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
-import os
 from django.core.asgi import get_asgi_application
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TMS.settings')
 
-application = get_asgi_application()  # just serve HTTP
+# Normal Django ASGI app
+application = get_asgi_application()
+
+# Add this for Vercel
+app = application  # <-- Vercel will now see 'app' as the entrypoint
