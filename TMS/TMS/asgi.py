96a3f70356c8534c3e53281c361ed4f75d61caa7
MisbahK4 <misbahpt6@gmail.com>
2025-09-11 19:47:30 +0530

    Prepare project for deployment: clean, tested, static collected

--- TMS/TMS/asgi.py
+++ TMS/TMS/asgi.py
@@ -8,17 +8,8 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-# import TMSapp
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import TMSapp.routing 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TMS.settings")
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(TMSapp.routing.websocket_urlpatterns)
-    )
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TMS.settings')
 
-})
+application = get_asgi_application()  # just serve HTTP
