18964b6e88bd704eadbefb972c2355b73e1fcca3
MisbahK4 <misbahpt6@gmail.com>
2025-09-15 16:55:03 +0530

    Add build script for Render

--- TMS/build.sh
+++ TMS/build.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
-set -o errexit
+set -o errexit  
 
+# Install backend dependencies
 pip install -r requirements.txt
 
+# Build frontend
+cd frontend
+npm install
+npm run build
+cd ..
+
+# Move React build into Django static files
+mkdir -p backend/static
+cp -r frontend/dist/* backend/static/
+
+# Collect Django static files
 python manage.py collectstatic --no-input
 
+# Run migrations
 python manage.py migrate
